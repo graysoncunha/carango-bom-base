93fc20d4beb704e4f18456e5fbefe72fd12311d7
Grayson Cunha <grayson.cunha@thoughtworks.com>
2021-08-19 19:01:02 -0300

    fix: inclui componente Browser Router no componente Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import CadastroMarca from '../pages/CadastroMarca'
 import CadastroVeiculo from '../pages/CadastroVeiculo'
@@ -8,13 +8,15 @@ import ListagemMarcas from '../pages/ListagemMarcas'
 
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="/" component={ListagemMarcas} />
-      <Route exact path="/cadastro-marca" component={CadastroMarca} />
-      <Route exact path="/alteracao-marca/:id" component={CadastroMarca} />
-      <Route exact path="/cadastro-veiculo" component={CadastroVeiculo} />
-      <Route exact path="/alteracao-veiculo/:id" component={CadastroVeiculo} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ListagemMarcas} />
+        <Route path="/cadastro-marca" component={CadastroMarca} />
+        <Route path="/alteracao-marca/:id" component={CadastroMarca} />
+        <Route path="/cadastro-veiculo" component={CadastroVeiculo} />
+        <Route path="/alteracao-veiculo/:id" component={CadastroVeiculo} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
