8a27cd7a44bdca2e5bac77934ec1d5e5941ad4f0
Laura Oliveira <lauralbbdo18@gmail.com>
2021-08-27 11:27:54 -0300

    feat: create table action buttons

---
+++ src/components/TableActionButtons/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IconButton, makeStyles } from '@material-ui/core'
+import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded'
+import EditRoundedIcon from '@material-ui/icons/EditRounded'
+
+const useStyles = makeStyles((theme) => ({
+  iconButton: {
+    color: 'rgb(0 0 0 / 25%)',
+    padding: 5,
+  },
+}))
+function TableActionButtons({ row, onClickDelete, onClickEdit }) {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <IconButton
+        aria-label="delete"
+        className={classes.iconButton}
+        onClick={() => onClickDelete(row)}
+        data-testid="deleteButton"
+      >
+        <DeleteOutlineRoundedIcon />
+      </IconButton>
+      <IconButton aria-label="edit" className={classes.iconButton} onClick={() => onClickEdit(row)}>
+        <EditRoundedIcon />
+      </IconButton>
+    </div>
+  )
+}
+
+export default TableActionButtons
