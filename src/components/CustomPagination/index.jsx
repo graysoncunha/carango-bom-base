5d1805d88e70a02fe2cb4db84001ff70515710a6
Laura Oliveira <lauralbbdo18@gmail.com>
2021-08-31 15:39:23 -0300

    feat: excluir veículo (#12)

---
+++ src/components/CustomPagination/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Pagination from '@material-ui/lab/Pagination'
+import PaginationItem from '@material-ui/lab/PaginationItem'
+import { useGridSlotComponentProps } from '@material-ui/data-grid'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiPaginationItem-page.Mui-selected': {
+      backgroundColor: 'rgba(109,70,228, 0.08)',
+    },
+  },
+}))
+
+function CustomPagination() {
+  const classes = useStyles()
+  const { state, apiRef } = useGridSlotComponentProps()
+
+  return (
+    <Pagination
+      className={classes.root}
+      variant="outlined"
+      shape="rounded"
+      page={state.pagination.page + 1}
+      count={state.pagination.pageCount}
+      renderItem={(props2) => <PaginationItem {...props2} disableRipple />}
+      onChange={(event, value) => apiRef.current.setPage(value - 1)}
+    />
+  )
+}
+
+export default CustomPagination
