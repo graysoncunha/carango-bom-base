fb60d6c651a859e9108f9e787752cab6c9dc6992
Laura Oliveira <lauralbbdo18@gmail.com>
2021-08-31 15:51:09 -0300

    fix: header links not working (#11)

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,18 +6,20 @@ import CadastroMarca from '../pages/CadastroMarca'
 import CadastroVeiculo from '../pages/CadastroVeiculo'
 import ListagemMarcas from '../pages/ListagemMarcas'
 import ListagemVeiculos from '../pages/ListagemVeiculos/index'
+import Header from '../components/Header'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ListagemMarcas} />
-        <Route path="/lista-veiculos" component={ListagemVeiculos} />
+        <Route path="/veiculos" component={ListagemVeiculos} />
         <Route path="/cadastro-marca" component={CadastroMarca} />
         <Route path="/alteracao-marca/:id" component={CadastroMarca} />
         <Route path="/cadastro-veiculo" component={CadastroVeiculo} />
         <Route path="/alteracao-veiculo/:id" component={CadastroVeiculo} />
       </Switch>
+      <Header />
     </BrowserRouter>
   )
 }
