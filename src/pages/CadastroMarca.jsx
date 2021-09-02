209ff5a3096bcd439180cc6aad07d57b70801931
Laura Oliveira <lauralbbdo18@gmail.com>
2021-09-02 16:46:10 -0300

    feat: create error treatment and feedback

--- src/pages/CadastroMarca.jsx
+++ src/pages/CadastroMarca.jsx
@@ -2,16 +2,22 @@ import { Button, TextField, makeStyles } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import useErros from '../hooks/useErros'
+import Alert from '@material-ui/lab/Alert'
 import MarcaService from '../services/MarcaService'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   actions: {
     top: '10px',
     marginRight: '10px',
   },
+  alert: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
 }))
 
 function CadastroMarca() {
+  const [{ status, error }, setStatus] = useState({ status: 'idle', error: null })
   const [marca, setMarca] = useState('')
 
   const classes = useStyles()
@@ -41,48 +47,64 @@ function CadastroMarca() {
     }
   }, [id]) // eslint-disable-line
 
+  async function cadastrar() {
+    try {
+      setStatus({ status: 'loading' })
+      await MarcaService.cadastrar({ nome: marca })
+      setMarca('')
+      history.goBack()
+      setStatus({ status: 'fulfilled' })
+    } catch (e) {
+      setStatus({ status: 'rejected', error: e.message })
+    }
+  }
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault()
-        if (possoEnviar()) {
-          if (id) {
-            MarcaService.alterar({ id, nome: marca }).then(() => {
-              history.goBack()
-            })
-          } else {
-            MarcaService.cadastrar({ nome: marca }).then(() => {
-              setMarca('')
-              history.goBack()
-            })
+    <>
+      {status === 'rejected' ? (
+        <Alert severity="error" className={classes.alert}>
+          {error}
+        </Alert>
+      ) : null}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          if (possoEnviar()) {
+            if (id) {
+              MarcaService.alterar({ id, nome: marca }).then(() => {
+                history.goBack()
+              })
+            } else {
+              cadastrar()
+            }
           }
-        }
-      }}
-    >
-      <TextField
-        value={marca}
-        onChange={(evt) => setMarca(evt.target.value)}
-        onBlur={validarCampos}
-        helperText={erros.marca.texto}
-        error={!erros.marca.valido}
-        name="marca"
-        id="marca"
-        label="Marca"
-        type="text"
-        variant="outlined"
-        fullWidth
-        required
-        margin="normal"
-      />
+        }}
+      >
+        <TextField
+          value={marca}
+          onChange={(evt) => setMarca(evt.target.value)}
+          onBlur={validarCampos}
+          helperText={erros.marca.texto}
+          error={!erros.marca.valido}
+          name="marca"
+          id="marca"
+          label="Marca"
+          type="text"
+          variant="outlined"
+          fullWidth
+          required
+          margin="normal"
+        />
 
-      <Button variant="contained" color="primary" type="submit" disabled={!possoEnviar()} className={classes.actions}>
-        {id ? 'Alterar' : 'Cadastrar'}
-      </Button>
+        <Button variant="contained" color="primary" type="submit" disabled={!possoEnviar()} className={classes.actions}>
+          {id ? 'Alterar' : 'Cadastrar'}
+        </Button>
 
-      <Button variant="contained" color="secondary" onClick={cancelar} className={classes.actions}>
-        Cancelar
-      </Button>
-    </form>
+        <Button variant="contained" color="secondary" onClick={cancelar} className={classes.actions}>
+          Cancelar
+        </Button>
+      </form>
+    </>
   )
 }
 
--- src/pages/CadastroVeiculo.jsx
+++ src/pages/CadastroVeiculo.jsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import { Button, TextField, MenuItem, makeStyles } from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert'
 import useErros from '../hooks/useErros'
 import VeiculoService from '../services/VeiculoService'
 import MarcaService from '../services/MarcaService'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   actions: {
     top: '10px',
     marginRight: '10px',
   },
+  alert: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
 }))
 
 function CadastroVeiculo() {
+  const [{ status, error }, setStatus] = useState({ status: 'idle', error: null })
   const [veiculo, setVeiculo] = useState({
-    marca: '',
+    marcaId: '',
     modelo: '',
     ano: '',
     valor: '',
@@ -71,7 +77,7 @@ function CadastroVeiculo() {
       const veiculo = await VeiculoService.consultar(id)
 
       setVeiculo({
-        marca: veiculo.marca.id,
+        marcaId: veiculo.marca.id,
         modelo: veiculo.modelo,
         ano: veiculo.ano,
         valor: veiculo.valor,
@@ -83,128 +89,144 @@ function CadastroVeiculo() {
     history.goBack()
   }
 
+  async function cadastrar() {
+    try {
+      setStatus({ status: 'loading' })
+      await VeiculoService.cadastrar({ ...veiculo })
+      setVeiculo({
+        marcaId: '',
+        modelo: '',
+        ano: '',
+        valor: '',
+      })
+      history.goBack()
+      setStatus({ status: 'fulfilled' })
+    } catch (e) {
+      setStatus({ status: 'rejected', error: e.message })
+    }
+  }
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault()
+    <div>
+      {status === 'rejected' ? (
+        <Alert severity="error" className={classes.alert}>
+          {error}
+        </Alert>
+      ) : null}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
 
-        if (possoEnviar()) {
-          if (id) {
-            VeiculoService.alterar({ id, ...veiculo }).then(() => {
-              history.goBack()
-            })
-          } else {
-            VeiculoService.cadastrar({ ...veiculo }).then(() => {
-              setVeiculo({
-                marca: '',
-                modelo: '',
-                ano: '',
-                valor: '',
+          if (possoEnviar()) {
+            if (id) {
+              VeiculoService.alterar({ id, ...veiculo }).then(() => {
+                history.goBack()
               })
-              history.goBack()
-            })
+            } else {
+              cadastrar()
+            }
           }
-        }
-      }}
-    >
-      <TextField
-        value={veiculo.marca}
-        onChange={(evt) =>
-          setVeiculo((prevState) => ({
-            ...prevState,
-            marca: evt.target.value,
-          }))
-        }
-        helperText={erros.marca.texto}
-        error={!erros.marca.valido}
-        name="marca"
-        id="marca"
-        label="Marca"
-        select
-        variant="outlined"
-        fullWidth
-        required
-        margin="normal"
+        }}
       >
-        {marcas &&
-          marcas.map(({ id, nome }) => (
-            <MenuItem key={id} value={id}>
-              {nome}
-            </MenuItem>
-          ))}
-      </TextField>
+        <TextField
+          value={veiculo.marcaId}
+          onChange={(evt) =>
+            setVeiculo((prevState) => ({
+              ...prevState,
+              marcaId: evt.target.value,
+            }))
+          }
+          helperText={erros.marca.texto}
+          error={!erros.marca.valido}
+          name="marca"
+          id="marca"
+          label="Marca"
+          select
+          variant="outlined"
+          fullWidth
+          required
+          margin="normal"
+        >
+          {marcas &&
+            marcas.map(({ id, nome }) => (
+              <MenuItem key={id} value={id}>
+                {nome}
+              </MenuItem>
+            ))}
+        </TextField>
 
-      <TextField
-        value={veiculo.modelo}
-        onChange={(evt) =>
-          setVeiculo((prevState) => ({
-            ...prevState,
-            modelo: evt.target.value,
-          }))
-        }
-        onBlur={validarCampos}
-        helperText={erros.modelo.texto}
-        error={!erros.modelo.valido}
-        name="modelo"
-        id="modelo"
-        label="Modelo"
-        type="text"
-        variant="outlined"
-        fullWidth
-        required
-        margin="normal"
-      />
+        <TextField
+          value={veiculo.modelo}
+          onChange={(evt) =>
+            setVeiculo((prevState) => ({
+              ...prevState,
+              modelo: evt.target.value,
+            }))
+          }
+          onBlur={validarCampos}
+          helperText={erros.modelo.texto}
+          error={!erros.modelo.valido}
+          name="modelo"
+          id="modelo"
+          label="Modelo"
+          type="text"
+          variant="outlined"
+          fullWidth
+          required
+          margin="normal"
+        />
 
-      <TextField
-        value={veiculo.ano}
-        onChange={(evt) =>
-          setVeiculo((prevState) => ({
-            ...prevState,
-            ano: evt.target.value,
-          }))
-        }
-        onBlur={validarCampos}
-        helperText={erros.ano.texto}
-        error={!erros.ano.valido}
-        name="ano"
-        id="ano"
-        label="Ano"
-        type="text"
-        variant="outlined"
-        fullWidth
-        required
-        margin="normal"
-      />
+        <TextField
+          value={veiculo.ano}
+          onChange={(evt) =>
+            setVeiculo((prevState) => ({
+              ...prevState,
+              ano: evt.target.value,
+            }))
+          }
+          onBlur={validarCampos}
+          helperText={erros.ano.texto}
+          error={!erros.ano.valido}
+          name="ano"
+          id="ano"
+          label="Ano"
+          type="text"
+          variant="outlined"
+          fullWidth
+          required
+          margin="normal"
+        />
 
-      <TextField
-        value={veiculo.valor}
-        onChange={(evt) =>
-          setVeiculo((prevState) => ({
-            ...prevState,
-            valor: evt.target.value,
-          }))
-        }
-        onBlur={validarCampos}
-        helperText={erros.valor.texto}
-        error={!erros.valor.valido}
-        name="valor"
-        id="valor"
-        label="Valor"
-        type="text"
-        variant="outlined"
-        fullWidth
-        required
-        margin="normal"
-      />
+        <TextField
+          value={veiculo.valor}
+          onChange={(evt) =>
+            setVeiculo((prevState) => ({
+              ...prevState,
+              valor: evt.target.value,
+            }))
+          }
+          onBlur={validarCampos}
+          helperText={erros.valor.texto}
+          error={!erros.valor.valido}
+          name="valor"
+          id="valor"
+          label="Valor"
+          type="text"
+          variant="outlined"
+          fullWidth
+          required
+          margin="normal"
+        />
 
-      <Button variant="contained" color="primary" type="submit" disabled={!possoEnviar()} className={classes.actions}>
-        {id ? 'Alterar' : 'Cadastrar'}
-      </Button>
+        <Button variant="contained" color="primary" type="submit" disabled={!possoEnviar()} className={classes.actions}>
+          {id ? 'Alterar' : 'Cadastrar'}
+        </Button>
 
-      <Button variant="contained" color="secondary" onClick={cancelar} className={classes.actions}>
-        Cancelar
-      </Button>
-    </form>
+        <Button variant="contained" color="secondary" onClick={cancelar} className={classes.actions}>
+          Cancelar
+        </Button>
+      </form>
+    </div>
   )
 }
 
