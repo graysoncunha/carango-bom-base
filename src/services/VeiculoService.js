13d3e73f731c29495f4127c988d19d965e57a773
Laura Oliveira <lauralbbdo18@gmail.com>
2021-09-03 10:40:32 -0300

    fix: fix code smells

--- src/services/VeiculoService.js
+++ src/services/VeiculoService.js
@@ -1,26 +1,26 @@
-import api from '../api/request'
+import request from '../api/request'
 
 const urlVeiculos = '/veiculos'
 
 const VeiculoService = {
   async cadastrar(veiculo) {
-    return await api(urlVeiculos, { method: 'POST', body: veiculo })
+    return request(urlVeiculos, { method: 'POST', body: veiculo })
   },
 
   async alterar(veiculo) {
-    return await api(`${urlVeiculos}/${veiculo.id}`, { method: 'PUT', body: veiculo })
+    return request(`${urlVeiculos}/${veiculo.id}`, { method: 'PUT', body: veiculo })
   },
 
   async consultar(id) {
-    return await api(`${urlVeiculos}/${id}`)
+    return request(`${urlVeiculos}/${id}`)
   },
 
   async listar() {
-    return await api(urlVeiculos)
+    return request(urlVeiculos)
   },
 
   async excluir(id) {
-    return await api(`${urlVeiculos}/${id}`, { method: 'DELETE' })
+    return request(`${urlVeiculos}/${id}`, { method: 'DELETE' })
   },
 }
 
