93f21dd8ce9792d0580c66d16c4981831b273a66
Laura Oliveira <lauralbbdo18@gmail.com>
2021-09-03 10:53:13 -0300

    Fix/resolve code smells (#20)

--- src/pages/CadastroMarca/CadastroMarca.jsx
+++ src/pages/CadastroMarca/CadastroMarca.jsx
@@ -40,12 +40,11 @@ function CadastroMarca() {
     history.goBack()
   }
 
-  // TODO: Avaliar remover disable na próxima linha
   useEffect(() => {
     if (id) {
       MarcaService.consultar(id).then((m) => setMarca(m.nome))
     }
-  }, [id]) // eslint-disable-line
+  }, [id])
 
   async function cadastrar() {
     try {
